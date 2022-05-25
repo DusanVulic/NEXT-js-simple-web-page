7949cfb8ae911207b7afb447c1be4d42734459d8
Dusan <dusanvulic11@gmail.com>
2022-05-25 11:36:02 +0200

    fetch data from JSON placeholder

--- pages/foxes/index.js
+++ pages/foxes/index.js
@@ -4,14 +4,35 @@ import styles from "../../styles/Foxes.module.css";
 
 import Head from "next/head";
 
-const Foxes = () => {
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return {
+    props: { foxes: data },
+  };
+};
+
+const Foxes = ({ foxes }) => {
   return (
     <>
       <Head>
         <title> Fox list | Foxes</title>
         <meta name="keywords" content="paperfox fox list about" />
       </Head>
-      <div> All Foxes </div>
+      <div>
+        <h3> All Foxes </h3>
+        {foxes.map((fox) => {
+          const { id, name } = fox;
+          return (
+            <div key={id}>
+              <a>
+                <h4>{fox.name}</h4>
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
