4a7a86b3a769dc4dd5355319412b3eef40587105
Dusan <dusanvulic11@gmail.com>
2022-05-25 12:09:51 +0200

    dinamyc routes setup

--- pages/foxes/index.js
+++ pages/foxes/index.js
@@ -3,6 +3,8 @@ import React from "react";
 import styles from "../../styles/Foxes.module.css";
 
 import Head from "next/head";
+//import Link from next
+import Link from "next/link";
 
 export const getStaticProps = async () => {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -25,11 +27,11 @@ const Foxes = ({ foxes }) => {
         {foxes.map((fox) => {
           const { id, name } = fox;
           return (
-            <div key={id}>
+            <Link href={`/foxes/${id}`} key={id}>
               <a className={styles.single}>
-                <h4>{fox.name}</h4>
+                <h4>{name}</h4>
               </a>
-            </div>
+            </Link>
           );
         })}
       </div>
